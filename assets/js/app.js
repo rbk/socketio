f7916f45d36dc48a88e11c6731dbaffbd973c2b0
Richard <richard.be.jamin@gmail.com>
2014-08-02 18:10:28 -0500

    this is getting even cooler

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,16 @@ $(function(){
 
 	// Send Message
 	$('form').submit(function(){
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
+		var message = $('#m').val();
+		if( message.length > 0 ){
+			socket.emit('chat message', message);
+			$('#m').val('');
+		} else {
+			// alert( 'no no no' );
+		}
 		return false;
 	});
+	
 	// Receive Message
 	socket.on('chat message', function(msg){
 		$('#messages').append($('<li>').text(username + ': ' +msg));
@@ -18,7 +24,7 @@ $(function(){
 	// Connected
 	socket.on('connected', function (data) {
 		// console.log( 'This object was broadcasted when you connected: ');
-		// console.log( data );
+		console.log( data );
 		for( i in data ){
 			$('#messages').append($('<li>').text(data[i].name + ': ' + data[i].message));
 		}
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 // Project name: SocketIO
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
-var port = process.env.PORT || 3001;
+var express      = require('express');
+var app          = express();
+var http         = require('http').Server(app);
+var io           = require('socket.io')(http);
+var mongoose     = require('mongoose');
+var cookieParser = require('cookie-parser');
+var port         = process.env.PORT || 3001;
+var router = express.Router();
+
+// Cookie Parser
+app.use(cookieParser('secret-string'));
+// app.use(function(req, res, next){
+//    res.end(JSON.stringify(req.cookies));
+// })
+
 
 // Log HTTP requests
 var logger = require('morgan');
@@ -23,13 +32,27 @@ var Chat = mongoose.model( 'Chat', {
     name: String,
     message: String
 });
-var Whatever = mongoose.model( 'Whatever', {});
+var Session = mongoose.model( 'Session', {
+    name: String,
+    ip: String,
+    message_count: Number,
+    time_connected: Number
+});
 
 // Static routes
-app.get('/', function(req, res){ res.sendfile('index.html'); });
-app.get('/chat', function(req, res){ res.sendfile('chat.html'); });
-app.get('/rproxy', function(req, res){ res.sendfile('how-to-setup-a-reverse-proxy-nginx.html'); });
-app.get('/template', function(req, res){ res.sendfile('template.html'); });
+app.get('/',            function(req, res){ res.sendfile('index.html'); });
+app.get('/chat',        function(req, res){ res.sendfile('chat.html'); });
+app.get('/rproxy',      function(req, res){ res.sendfile('how-to-setup-a-reverse-proxy-nginx.html'); });
+app.get('/template',    function(req, res){ res.sendfile('template.html'); });
+
+
+// API
+app.get('/api/:id?', function(req, res, next) {
+    // ... maybe some additional /bar logging ...
+    console.log( 'you hit our api' );
+    res.end(JSON.stringify( req.params.id ));
+    // next();
+});
 
 app.use(express.static('assets'));
 app.use(express.static('public'));
@@ -37,6 +60,20 @@ app.use(express.static('public'));
 // Connection made to socket
 io.on('connection', function(socket){
 
+    //:: On connect
+    // 1. ask for nick name
+    // 2. create cooke with name
+    // 3. store name in database
+    // 4. add send name to client
+    // 5. add name to user list on client
+
+    // :: Leave chat
+    // 1. remove user from database
+    // 2. update user list on clients
+
+    //:: Return to chat
+    // 1. check for cookie
+    // 2.  if cookie send chat room
 
     // Get ALL messages
     Chat.find({ },function (err, messages) {
@@ -45,13 +82,14 @@ io.on('connection', function(socket){
         socket.emit('connected', messages);
     });
 
-    console.log( 'Connection MADE!!!!!!!!!!!!>>>>>>>>>>>' )
-    var new_connection = new Whatever({ this: 'Zildjian', that: 'blah', other: 'abc123' });
-    new_connection.save(function (err) {
-        if( err ){
-            console.log( err )
-        }
-    });
+    // var session = new User({ name: '-', ip: 'ip'  });
+    // session.save(function (err) {
+    //     if( err ){
+    //         console.log( err )
+    //     }
+    // });
+
+// console.log( socket.handshake.headers.cookie )
 
 
     // Someone sends a message
@@ -71,9 +109,14 @@ io.on('connection', function(socket){
     });
 
 
+    // On disconnect
+    socket.on('disconnect', function( res ){
+        console.log( res )
+    });
 
 
 }); // end io connect
+
 
 
 // Start server
